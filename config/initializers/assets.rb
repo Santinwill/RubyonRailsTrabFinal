e317897f3bfa2b2e73e4517cd1b36fec457f97ff
“SantinWill” <SantinWill>
2023-11-22 19:56:46 -0300

    materialize

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( cable.js catastrophes.js  people.js  recipients.js  scaffolds.js
+                                                           catastrophes.css people.css recipients.css scaffolds.css
+                                                )
